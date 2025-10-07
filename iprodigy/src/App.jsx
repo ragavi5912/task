1ff0aac0b7ecd0620c2ffa19bb7dad822876ded4
ragavifuiox <ragavi.fuiox@gmail.com>
2025-10-07 13:39:23 +0530

    Fix import case for Why.jsx

--- iprodigy/src/App.jsx
+++ iprodigy/src/App.jsx
@@ -8,8 +8,8 @@ import About from "./components/about/about.jsx";
 import Content from "./components/content/content.jsx";
 import Home from "./components/home/home.jsx"; 
 import Main from "./components/main/mains.jsx";
-import Why from "./components/why-us/Why.jsx";
 
+import Why from "./components/why-us/why.jsx";
 
 
 function App() {
